e1b0a2a439ab8843edf5f1fba643b769e1729238
ClaudeRuxpair <165365213+DotBlossom@users.noreply.github.com>
2024-12-11 01:14:57 +0900

    Create AsyncPropagationsPage.jsx

---
+++ react-prototype/src/pages/AsyncPropagationsPage.jsx
@@ -0,0 +1,47 @@
+
+import React, {useRef} from 'react';
+import axios from 'axios';
+
+
+export default function AsyncPropagationsPage() {
+
+  const invokeRef = useRef(false);
+ 
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5050/ai-api/inference/result/1`
+      );
+      console.log('first is on');
+      invokeRef.current = response.data;
+      
+    } catch (e) {
+      console.log(e);
+    }
+    invoke_server(invokeRef);
+  };
+  
+  const invoke_server = async (req) => {
+    try {
+      console.log(req.current);
+      const mx = req.current.id;
+      // if id == 10 -> retrieved by first data 가져옴
+
+      const response = await axios.get(`http://localhost:5050/ai-api/user/${mx}`);
+      console.log(response.data);
+      console.log("invoked");
+      
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+
+  return (
+    <div className="App">
+      <button onClick={handleSubmit}>Submit</button>
+      <p>t</p>
+    </div>
+  );
+}
+
